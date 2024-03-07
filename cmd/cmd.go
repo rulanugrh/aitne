d033922b4315fd33fa4d9a026698ac78009b5da9
Pradana <nextarituenak01@gmail.com>
2024-03-07 16:54:12 +0000

    chore: prettier for file cmd.go

--- cmd/cmd.go
+++ cmd/cmd.go
@@ -11,74 +11,74 @@ import (
 )
 
 var (
-  opt = flag.String("opt", "get", "to use operator ex. get / delete / catch")
-  types = flag.String("types", "deployment", "to use type data ex. deployment / daemonset")
-  name = flag.String("name", "demo-deployment", "this flag use for catch opt ")
+	opt   = flag.String("opt", "get", "to use operator ex. get / delete / catch")
+	types = flag.String("types", "deployment", "to use type data ex. deployment / daemonset")
+	name  = flag.String("name", "demo-deployment", "this flag use for catch opt ")
 )
 
 var (
-  get = "get"
-  catch = "catch"
-  deleted = "delete"
+	get     = "get"
+	catch   = "catch"
+	deleted = "delete"
 )
 
 type CLI struct {
-  deployment service.Deployment
-  daemon service.DaemonSet
+	deployment service.Deployment
+	daemon     service.DaemonSet
 }
 
 func main() {
 	deployment, daemon, _, err := config.GetConfig()
-  if err != nil {
-    log.Printf("Error %s", err.Error())
-  }
+	if err != nil {
+		log.Printf("Error %s", err.Error())
+	}
 
-  newDeployment := service.NewDeployment(deployment)
-  newDaemonSet := service.NewDaemonSet(daemon)
-  c := CLI{
-    deployment: newDeployment,
-    daemon: newDaemonSet,
-  }
+	newDeployment := service.NewDeployment(deployment)
+	newDaemonSet := service.NewDaemonSet(daemon)
+	c := CLI{
+		deployment: newDeployment,
+		daemon:     newDaemonSet,
+	}
 
-  if opt == &get {
-    c.get_opt(*types)
-  } else if opt == &catch {
-    c.catch_opt(*types, *name)
-  } else {
-    log.Println("your opt is invalid, use go run cmd.go -h to see details")
-  }
+	if opt == &get {
+		c.get_opt(*types)
+	} else if opt == &catch {
+		c.catch_opt(*types, *name)
+	} else {
+		log.Println("your opt is invalid, use go run cmd.go -h to see details")
+	}
 }
 
 func (c *CLI) get_opt(types string) {
-  if types == "deployment" {
-    convert_response_to_json(c.deployment.List())
-  } else if types == "daemon" {
-    convert_response_to_json(c.daemon.List())
-  } else {
-    log.Println("sorry invalid type data")
-  }
+	if types == "deployment" {
+		convert_response_to_json(c.deployment.List())
+	} else if types == "daemon" {
+		convert_response_to_json(c.daemon.List())
+	} else {
+		log.Println("sorry invalid type data")
+	}
 }
 
 func (c *CLI) catch_opt(types string, name string) {
-  if types == "deployment" {
-    convert_response_to_json(c.deployment.GetByName(name))
-  } else if types == "daemon" {
-    convert_response_to_json(c.daemon.GetByName(name))
-  } else {
-    log.Println("sorry invalid type data")
-  }
+	if types == "deployment" {
+		convert_response_to_json(c.deployment.GetByName(name))
+	} else if types == "daemon" {
+		convert_response_to_json(c.daemon.GetByName(name))
+	} else {
+		log.Println("sorry invalid type data")
+	}
 }
 
 func convert_response_to_json(data any, err error) {
-  if err != nil {
-    log.Printf("something error %s", err.Error())
-  }
+	if err != nil {
+		log.Printf("something error %s", err.Error())
+	}
 
-  response, err := json.Marshal(data)
-  if err != nil {
-    log.Printf("errror marshal json %s", err.Error())
-  }
+	response, err := json.Marshal(data)
+	if err != nil {
+		log.Printf("errror marshal json %s", err.Error())
+	}
 
-  fmt.Println(string(response))
-  
+	fmt.Println(string(response))
+
 }
