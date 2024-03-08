ce83f7fcbfdb6859c4b70e3e603404c4a296278e
Pradana <nextarituenak01@gmail.com>
2024-03-08 12:48:59 +0000

    chore: adding response for utils

---
+++ internal/util/response.go
@@ -0,0 +1,47 @@
+package util
+
+type Response struct {
+	Code    int    `json:"code"`
+	Message string `json:"msg"`
+	Data    any    `json:"data"`
+}
+
+func (r Response) Error() string {
+	return r.Message
+}
+
+func Success(msg string, data any) error {
+	return Response{
+		Code:    200,
+		Message: msg,
+		Data:    data,
+	}
+}
+
+func Created(msg string) error {
+	return Response{
+		Code:    201,
+		Message: msg,
+	}
+}
+
+func NotFound(msg string) error {
+	return Response{
+		Code:    404,
+		Message: msg,
+	}
+}
+
+func BadRequest(msg string) error {
+	return Response{
+		Code:    400,
+		Message: msg,
+	}
+}
+
+func InternalServerError(msg string) error {
+	return Response{
+		Code:    500,
+		Message: msg,
+	}
+}
