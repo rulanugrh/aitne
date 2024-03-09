bb85cd84710d07866e3ce6f83bfffb0698a96efa
Pradana <nextarituenak01@gmail.com>
2024-03-09 16:33:13 +0000

    feat: adding sub router for core endpoint

--- api/api.go
+++ api/api.go
@@ -7,8 +7,11 @@ import (
 
 	"github.com/gorilla/mux"
 	"github.com/rulanugrh/aitne/api/apps"
+	"github.com/rulanugrh/aitne/api/core"
 	"github.com/rulanugrh/aitne/internal/config"
 	srv "github.com/rulanugrh/aitne/internal/service/apps"
+	srv2 "github.com/rulanugrh/aitne/internal/service/core"
+
 )
 
 var (
@@ -20,6 +23,10 @@ type API struct {
 	daemon     apps.DaemonEndpoint
 	replica    apps.ReplicaEndpoint
 	statefull  apps.StatefullEndpoint
+
+	namespace core.NamespaceEndpoint
+	pod       core.PodEndpoint
+	service   core.ServiceEndpoint
 }
 
 func main() {
@@ -33,11 +40,19 @@ func main() {
 	replica := srv.NewReplicaSet(client)
 	statefull := srv.NewStatefulSet(client)
 
+	pod := srv2.NewPod(client)
+	namespace := srv2.NewNamespace(client)
+	service := srv2.NewServiceKurbenetes(client)
+
 	api := API{
 		statefull:  apps.NewStatefullEndpoint(statefull),
 		daemon:     apps.NewDameonEndpoint(daemon),
 		replica:    apps.NewReplicaEndpoint(replica),
 		deployment: apps.NewDeploymentEndpoint(deployment),
+
+		pod: core.NewPodEndpoint(pod),
+		namespace: core.NewNamespaceEndpoint(namespace),
+		service: core.NewServiceEndpoint(service),
 	}
 
 	router := mux.NewRouter()
@@ -45,6 +60,10 @@ func main() {
 	api.DeploymentRoute(router)
 	api.ReplicaRouter(router)
 	api.StatefullRouter(router)
+
+	api.PodRouter(router)
+	api.NamespaceRouter(router)
+	api.ServiceRouter(router)
 
 	server := http.Server{
 		Addr:    "0.0.0.0:3000",
@@ -90,4 +109,28 @@ func (api *API) StatefullRouter(r *mux.Router) {
 	app.HandleFunc("/getAll/", api.statefull.Get).Methods("GET")
 	app.HandleFunc("/get/{name}", api.statefull.GetByName).Methods("GET")
 	app.HandleFunc("/delete/{name}", api.statefull.Delete).Methods("DELETE")
+}
+
+func (api *API) PodRouter(r *mux.Router) {
+	app := r.PathPrefix("/api/pod").Subrouter()
+	app.HandleFunc("/create/", api.pod.Create).Methods("POST")
+	app.HandleFunc("/getAll/", api.pod.Get).Methods("GET")
+	app.HandleFunc("/get/{name}", api.pod.GetByName).Methods("GET")
+	app.HandleFunc("/delete/{name}", api.pod.Delete).Methods("DELETE")
+}
+
+func (api *API) NamespaceRouter(r *mux.Router) {
+	app := r.PathPrefix("/api/namespace").Subrouter()
+	app.HandleFunc("/create/", api.namespace.Create).Methods("POST")
+	app.HandleFunc("/getAll/", api.namespace.Get).Methods("GET")
+	app.HandleFunc("/get/{name}", api.namespace.GetByName).Methods("GET")
+	app.HandleFunc("/delete/{name}", api.namespace.Delete).Methods("DELETE")
+}
+
+func (api *API) ServiceRouter(r *mux.Router) {
+	app := r.PathPrefix("/api/service").Subrouter()
+	app.HandleFunc("/create/", api.service.Create).Methods("POST")
+	app.HandleFunc("/getAll/", api.service.Get).Methods("GET")
+	app.HandleFunc("/get/{name}", api.service.GetByName).Methods("GET")
+	app.HandleFunc("/delete/{name}", api.service.Delete).Methods("DELETE")
 }
